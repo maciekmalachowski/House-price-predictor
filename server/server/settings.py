7f464d31296c4295cd47a38150b926e7a2db1bda
MaciekEO <maciejmalachowski1@gmail.com>
2022-07-06 12:41:54 +0200

    some changes

--- server/server/settings.py
+++ server/server/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yvv9@ud0fh-y=wi4f5qj4)hkznvdg$o(+bvwzbcq*aw5pofvhi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
