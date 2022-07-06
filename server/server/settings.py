2c90190a1ff5432500b018805fc85495192e8ce3
MaciekEO <maciejmalachowski1@gmail.com>
2022-07-06 12:07:01 +0200

    added whitenoise

--- server/server/settings.py
+++ server/server/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static/images')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 django_heroku.settings(locals())
 
 MEDIA_URL = '/images/'
