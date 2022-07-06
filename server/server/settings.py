872005728579bef4b65e46d77bb76ffb7998366b
MaciekEO <maciejmalachowski1@gmail.com>
2022-07-06 11:05:01 +0200

    Heroku deploy

--- server/server/settings.py
+++ server/server/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-yvv9@ud0fh-y=wi4f5qj4)hkznvdg$o(+bvwzbcq*aw5pofvhi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +124,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+django_heroku.settings(locals())
 
 MEDIA_URL = '/images/'
 
